9c40b5bb12b839e5edbeb51268fcb53b08c3a446
jwprillaman <jwprillaman@gmail.com>
2016-03-07 20:52:06 -0700

    serving html

--- app.go
+++ app.go
@@ -1,14 +1,23 @@
 package main
 
 import  (
+	"html/template"
 	"fmt"
 	"net/http"
+	"path"
 )
-
+type User struct {
+	id int
+	name string
+}
 func Index(w http.ResponseWriter, r *http.Request){
 	r.ParseForm()
-	fmt.Println(r.Form)
-	fmt.Fprintf(w, "Hiya")
+	user := User{0,"jim"}
+	fmt.Println("New User Connected")
+	fp := path.Join("assets", "html", "index.html")
+	templ, err := template.ParseFiles(fp)
+	err = templ.Execute(w,user)
+	if err != nil { fmt.Println("error occurred")}
 }
 
 func main(){
