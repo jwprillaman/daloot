af1637d6d5696bee4e3baa23de99853fe3395722
jwprillaman <jwprillaman@gmail.com>
2016-03-09 23:03:42 -0700

    quick first pass at structs for mongodb

--- app.go
+++ app.go
@@ -5,15 +5,48 @@ import  (
 	"fmt"
 	"net/http"
 	"path"
+	"labix.org/v2/mgo.v2.a"
+	"labix.org/v2/mgo/bson"
+	"time"
 )
+
 type User struct {
-	id int
-	name string
+	Id 'bson:"_id,omitempty"'
+	Name string
+	Avatar string
 }
+
+type Stats struct {
+	Viewed int64
+	TotalStay time.Time
+	AvgStay time.Time
+	Offers int64
+}
+
+type Visitor struct {
+	Start time.Time
+	User int64
+
+}
+type Offer struct {
+	LastAction time.Time
+	For int64
+	From int64
+}
+
+type Doc struct {
+	ID bson.ObjectId 'bson:"_id,omitempty"'
+	Type string
+	Timestamp time.Time
+	Owner int64
+	Watchers []int64
+	Offers Offer
+	Stats Stats
+}
+
 func Index(w http.ResponseWriter, r *http.Request){
 	r.ParseForm()
-	user := User{0,"jim"}
-	fmt.Println("New User Connected")
+	user := User{0,"anon","http://www.imgur.com/Ot13hl"}
 	fp := path.Join("assets", "html", "index.html")
 	templ, err := template.ParseFiles(fp)
 	err = templ.Execute(w,user)
