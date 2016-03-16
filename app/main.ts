7cb2ee41b08114afd8c2f1e3c7b8cc81a87b2c6e
jwprillaman <jwprillaman@gmail.com>
2016-03-15 22:20:59 -0600

    go style structure

---
+++ app/main.ts
@@ -0,0 +1,4 @@
+import {bootstrap}    from 'angular2/platform/browser'
+import {AppComponent} from './app.component'
+import {ANGULAR2_GOOGLE_MAPS_PROVIDERS} from 'angular2-google-maps/core';
+bootstrap(AppComponent, [ANGULAR2_GOOGLE_MAPS_PROVIDERS]);
